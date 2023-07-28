26fdfbf8ed4c66b01fa20f712817d2f9892976ec
Hermione Dadheech <hermionedadheech@gmail.com>
2023-07-29 01:14:15 +0530

    Shree ganesh

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <img src="" alt="Home Icon"/>
+            <img src="" alt="Search Icon"/>
+            <img src="" alt="Create Icon"/>
+            <img src="" alt="Reels Icon"/>
+            <img src="" alt="Profile Icon"/>
+        </footer>
+    );
+};
+
+export default Footer;
