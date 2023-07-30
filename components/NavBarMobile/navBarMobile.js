470611131306027f16b05b04fdd88fddd87c1e8d
Hermione Dadheech <hermionedadheech@gmail.com>
2023-07-30 15:03:57 +0530

    navBarMobile

--- components/NavBarMobile/navBarMobile.js
+++ components/NavBarMobile/navBarMobile.js
@@ -5,40 +5,20 @@ import Home from '../../assets/img/svg/home.png';
 import Search from '../../assets/img/svg/search.png';
 import Create from '../../assets/img/svg/create.png';
 import Reels from '../../assets/img/svg/reels.png';
+// import Message from '../../assets/img/svg/message.png';
+import Profile from '../../assets/img/svg/profile-picture.jpg';
 
 const NavBarMobile = () => {
     return (
         <div className={styles.container}>
-            NavBarMobile
-            <footer className={styles.footer}>
-            <Image className={styles.icon}
-        src={Home}
-        width={24}
-        height={24}
-        alt="Home"
-      />
-        <Image className={styles.icon}
-        src={Search}
-        width={24}
-        height={24}
-        alt="Search"
-      />
-        <Image className={styles.icon}
-        src={Create}
-        width={24}
-        height={24}
-        alt="Create"
-      />
-        <Image className={styles.icon}
-        src={Reels}
-        width={24}
-        height={24}
-        alt="Reels"
-      />
-           
-            <ProfileSection post={{username:'test'}} ShowName={false} />
-            <img src="" alt="Profile Icon"/>
-        </footer>
+                <Image className={styles.icon} src={Home} width={24} height={24} alt="Home" />
+                <Image className={styles.icon} src={Search} width={24} height={24} alt="Search" />
+                <Image className={styles.icon} src={Create} width={24} height={24} alt="Create" />
+                <Image className={styles.icon} src={Reels} width={24} height={24} alt="Reels" />
+       
+                {/* <Image className={styles.icon} src={Message} width={24} height={24} alt="Message" /> */}
+                {/* <Image className={styles.icon} src={Profile} width={24} height={24} alt="Profile" /> */}
+                <ProfileSection post={{username:'test'}} ShowName={false} />
         </div>
     );
 };
