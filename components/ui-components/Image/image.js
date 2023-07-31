413c12bbedf4c6caea0903ae519b6ed26d0ef2c2
Hermione Dadheech <hermionedadheech@gmail.com>
2023-07-31 20:49:11 +0530

    UI changes

--- components/ui-components/Image/image.js
+++ components/ui-components/Image/image.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from "./image.module.css"
+import Image from 'next/image';
 
-const ImageSection = ({url}) => (
-  <div className={styles.image}>
-    <img src={url} />
-  </div> 
-);
+const ImageSection = ({url, hashUrl,height,width,isMobileView}) => {
+  const heightImg = height ? height/10: 200
+  const widthImg = width ? width/10:200
+  return (
+    <div className={styles.image}>
+      <Image src={url} blurDataURL={hashUrl} height={heightImg} width={widthImg} />
+    </div>
+  )
+};
 
 export default ImageSection;
